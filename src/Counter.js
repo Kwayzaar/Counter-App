459243aeb8d873dff9e12d9a96b3d08fd7d7650e
eric <lee.eric.03@gmail.com>
2021-07-28 22:01:20 -0600

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+
+function Counter() {
+  const [count, setCount] = useState(0)
+
+  const incrementCount = () => {
+    setCount(count + 1)
+  }
+
+  const decrementCount = () => {
+    setCount(count - 1)
+  }
+
+  const resetCount = () => {
+    setCount(0)
+  }
+
+  const countSeconds = () => {
+    setCount(count += 1)
+  }
+
+  const startTimer = () => {
+    setCount(setInterval(1000))
+  }
+
+    return(
+      <div>
+        <h1>Count!</h1>
+        <p>{count}</p>
+        <div className="buttons">
+          <div className="manualCount">
+            <button onClick={incrementCount}>+</button>
+            <button onClick={decrementCount}>-</button>
+          </div>
+          <button onClick={countSeconds}>Count Seconds</button>
+          <button onClick={resetCount}>Reset</button>
+        </div>
+      </div>
+  )
+}
+
+export default Counter
